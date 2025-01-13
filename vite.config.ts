58a6216bdaf4c8417518d325976874821a180e5f
a.irkhin <a.irkhin@fun-box.ru>
2025-01-14 00:18:58 +0300

    add deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/jora/',
   plugins: [react()],
   resolve: {
     alias: [{ find: 'src', replacement: resolve(__dirname, 'src') }],
